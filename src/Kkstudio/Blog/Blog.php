033a71efc9af582a8fc460989c024ab3b1be46ef
chris15001900 <krzysztofbaczkiewicz@gmail.com>
2014-10-29 19:17:49 +0100

    bug fixes

--- src/Kkstudio/Blog/Blog.php
+++ src/Kkstudio/Blog/Blog.php
@@ -1,19 +1,24 @@
 <?php namespace Kkstudio\Blog;
 
-use Repositories\BlogRepository;
-
 class Blog extends \App\Module {
+
+	protected $repo;
+
+	public function __construct() 
+	{
+		$this->repo = new Repositories\BlogRepository;
+	}
 
 	public function posts(BlogRepository $repo) 
 	{
-		$posts = $repo->posts();
+		$posts = $this->repo->posts();
 
 		return $posts;
 	}
 
 	public function post($slug, BlogRepository $repo) 
 	{
-		$post = $repo->post($slug);
+		$post = $this->repo->post($slug);
 
 		return $post;
 	}
